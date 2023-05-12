eca2cc03f39fbb8a0e2f6dad479f2ebe717e0c6b
amorales <amorales@sirt.com>
2023-05-12 16:13:53 +0200

    v1

--- examples/fgt-ha_onramp_xlb/output.tf
+++ examples/fgt-ha_onramp_xlb/output.tf
@@ -9,14 +9,6 @@ output "fgt" {
   }
 }
 
-output "vm_spoke" {
-  value = {
-    admin_user = split("@", data.google_client_openid_userinfo.me.email)[0]
-    pip        = module.vm_spoke.vm["pip"]
-    ip         = module.vm_spoke.vm["ip"]
-  }
-}
-
 output "faz" {
   value = {
     mgmt_url   = "https://${module.faz.faz_public-ip}"
